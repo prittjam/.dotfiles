5c512e0c927880830e8e71d38cbee171a365304f
James Pritts <prittjam@cmp.felk.cvut.cz>
2016-05-24 11:48:27 +0200

    changes to init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,22 +1,32 @@
 ;; add elisp directory to path
-(add-to-list 'load-path "~/.emacs.d/elisp/")
-(let ((default-directory  "~/.emacs.d/elisp/"))
-  (normal-top-level-add-subdirs-to-load-path))
+;;(add-to-list 'load-path "~/.emacs.d/elisp/")
 
 ;; setup MELPA
 (require 'package)
+
 (add-to-list 
  'package-archives
  '("melpa" . "https://melpa.org/packages/"))
 
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-
 (package-initialize)
 
+;; add packages not in melpa for emacs23 for lcraid
+(when (< emacs-major-version 24)
+  (let ((default-directory  "~/.emacs.d/elisp/"))
+    (normal-top-level-add-subdirs-to-load-path))
+
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+  (progn;
+    (add-to-list 'custom-theme-load-path "~/.emacs.d/elisp/emacs-color-theme-solarized"))
+  
+  ;; --- MATLAB MODE ---
+  (load-library "~/.emacs.d/elisp/matlab-emacs/matlab-load.el"))
+
+;; setup macros, preferences
 (setq column-number-mode t) 
 (global-set-key [f9] 'recompile)
+(load-theme 'solarized t)
 
 ;; setup speedbar
 (require 'sr-speedbar)
@@ -34,15 +44,7 @@
     (setq window-size-fixed 'width))
   ))
 
-
-(if (>= emacs-major-version 24)
-    (progn;
-      (add-to-list 'custom-theme-load-path "~/.emacs.d/elisp/emacs-color-theme-solarized")
-      (load-theme 'solarized t)))
-
-;; --- MATLAB MODE ---
-(load-library "~/.emacs.d/elisp/matlab-emacs/matlab-load.el")
-
+;; setup MATLAB 
 (custom-set-variables
- '(matlab-shell-command "matlab81")
+ '(matlab-shell-command "matlab9")
  '(matlab-shell-command-switches '("-nodesktop -nosplash")))
